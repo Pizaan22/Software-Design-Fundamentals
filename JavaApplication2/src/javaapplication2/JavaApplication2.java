de011e5c48518d79ff3231e6e30d4ebbfa4812ea
Pizaan22 <105440452+Pizaan22@users.noreply.github.com>
2022-05-26 09:49:24 -0400

    Added hello world! :0

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -2,6 +2,7 @@ package javaapplication2;
 public class JavaApplication2 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        //Hello World!
         //Commit vs Save
     }
 }
