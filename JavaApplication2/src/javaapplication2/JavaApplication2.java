c4556dac28aefaed0b0222423f371c6c015305ba
Pizaan22 <105440452+Pizaan22@users.noreply.github.com>
2022-05-26 10:21:44 -0400

    Update JavaApplication2.java

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -3,6 +3,7 @@ public class JavaApplication2 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         String fetch = "yes";
+        System.out.print(fetch);
         //Hello World!
         //Commit vs Save
     }
