6827ed3f93176b8b723f599c71effbf8339dfb4e
pizaa <pizaa@Pizaan>
2022-05-26 09:42:56 -0400

    Comment added

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -2,5 +2,6 @@ package javaapplication2;
 public class JavaApplication2 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        //Commit vs Save
     }
 }
