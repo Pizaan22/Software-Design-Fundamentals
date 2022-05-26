fcee30ca0f9977d59c310bc302fe8a2c830d5e91
pizaa <pizaa@Pizaan>
2022-05-26 10:18:57 -0400

    DataType modified

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -2,7 +2,7 @@ package javaapplication2;
 public class JavaApplication2 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        int fetch = "yes";
+        String fetch = "yes";
         //Hello World!
         //Commit vs Save
     }
