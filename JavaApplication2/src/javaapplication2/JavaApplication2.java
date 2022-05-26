483080529ed8000061c997f43a17b749d40fd96b
Pizaan22 <105440452+Pizaan22@users.noreply.github.com>
2022-05-26 10:15:08 -0400

    Version 2.0

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -2,6 +2,7 @@ package javaapplication2;
 public class JavaApplication2 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        int fetch = "yes";
         //Hello World!
         //Commit vs Save
     }
